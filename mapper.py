98fdbd2b273a2f88631353d0e61852b0fb4dd78e
eabgupt <abhi03abhishekgupta@gmail.com>
2019-09-17 18:41:03 +0000

    lowecase text and remove stopwords

--- mapper.py
+++ mapper.py
@@ -5,10 +5,19 @@ import sys
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-
+   # make lower case
+    line = line.lower()
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    no_punct = ""
+    for char in line:
+      if char not in punctuations:
+       no_punct = no_punct + char 
     # split the line into words; splits on any whitespace
-    words = line.split()
-
+    words = no_punct.split()
+    stopwords = set(['the' , 'and' ])
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+      if word not in stopwords:
+       print '%s\t%s' % (word, "1")
+
+    
